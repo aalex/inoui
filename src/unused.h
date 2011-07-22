6d28bd85076dde49aa60e95576d3d013ad3a4462
Alexandre Quessy <alexandre@quessy.net>
2011-07-23 00:01:50 +0200

    add fudi_sender.{h,cpp}

---
+++ src/unused.h
@@ -0,0 +1,9 @@
+#ifndef __UNUSED_H__
+#define __UNUSED_H__
+
+/** @file
+ * Macro to avoid errors due to unused parameters to functions.
+ */
+#define UNUSED(x) ( (void)(x) )
+
+#endif // __UNUSED_H__
