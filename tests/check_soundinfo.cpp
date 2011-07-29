752679e2f1c459685bbeb97f8957db8de16b202b
Alexandre Quessy <alexandre@quessy.net>
2011-07-29 12:04:00 +0200

    fix a test

--- tests/check_soundinfo.cpp
+++ tests/check_soundinfo.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    long int duration = get_sound_file_duration("test.wav");
+    long int duration = inoui::get_sound_file_duration("test.wav");
     std::cout << "Duration is " << duration << " ms" << std::endl;
     return 0;
 }
